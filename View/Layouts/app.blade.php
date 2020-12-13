7a3c675451bc6038f08d01373a66efb8b1d8a159
kractus101 <67240838+kractus101@users.noreply.github.com>
2020-12-14 01:10:54 +0530

    Create app.blade.php

---
+++ View/Layouts/app.blade.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+    <title>Contact Us</title>
+</head>
+
+<body>
+    <div class='container'>
+
+        @yield ('content')
+
+    </div>
+
+
+
+    @yield('footer')
+
+</body>
+
+</html>
